36ff7facc19b5ea511d3186c49d86400940c801f
Gaston <gastonallerdev@gmail.com>
2024-04-11 14:29:02 -0300

    Navbar&responsive&icon

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
@@ -7,29 +7,90 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const toggleNavBar = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
+  const links = [
+    { label: "Home", href: "/home" },
+    { label: "About", href: "/about" },
+    { label: "Characters", href: "/characters" },
+    { label: "FAQ", href: "/faq" },
+  ];
 
   return (
-    <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-gray-500 text-black">
-      <div className="flex items-center mb-4 md:mb-0">
-        <Link href="/">
-          <Image
-            src="/icons/icon.png"
-            alt="Descripción de la imagen"
-            width={200}
-            height={200}
-          />
-        </Link>
+    <nav className="bg-black">
+      <div className="max-w-7xl p-3 mx-auto px-4  sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 ">
+              <Image
+                src="/icons/icon.png"
+                alt="Descripción de la imagen"
+                width={180}
+                height={180}
+              />
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center space-x-4 link-container-2">
+              {links.map((link, index) => (
+                <Link key={index} href={link.href}>
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="md:hidden flex items-center ">
+            <button
+              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              onClick={toggleNavBar}
+            >
+              {isOpen ? (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="https://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="h-6 w-6"
+                  xmlns="https://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="white"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16m-7 6h7"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="flex items-center gap-6 text-xl links-container">
-        <Link href="/characters">Characters</Link>
-        <Link href="/history">History</Link>
-        <Link href="/about">FAQ</Link>
-        <Link href="/FAQ">About</Link>
-        <button className="md:hidden" onClick={toggleNavBar}>{isOpen ?  "t" : "hidden"} </button>
-      </div>
+      {isOpen && (
+        <div className="md:hidden mt-3 ">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 transition-all duration-300 ease-in-out link-container-3 ">
+            {links.map((link, index) => (
+              <Link key={index} href={link.href}>
+                {link.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
